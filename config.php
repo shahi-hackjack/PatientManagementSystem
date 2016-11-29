c60ade48cc7d9cd53db8a592f670b67e611da72c
MOHAMMAD SHAHENOOR <contactshahenoor@gmail.com>
2016-11-30 00:58:16 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,17 +1,17 @@
-<?php 
+<?php
 
 $servername = "localhost";
-$username = "root";
-$password = "";
+$username   = "root";
+$password   = "";
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password,"patientdir");
+$conn = mysqli_connect($servername, $username, $password, "patientdir");
 
 // Check connection
 if (!$conn) {
-    echo json_encode(die("Connection failed: " . mysqli_connect_error())) ;
+    echo json_encode(die("Connection failed: " . mysqli_connect_error()));
 }
 
-else 
-	//echo json_encode("connection estaASDSAblished ");
+//else 
+//echo json_encode("connection estaASDSAblished ");
 ?>
