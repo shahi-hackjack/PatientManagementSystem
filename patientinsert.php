5631565493978fd5002526106a2a061212090e2c
MOHAMMAD SHAHENOOR <contactshahenoor@gmail.com>
2016-11-30 01:03:45 +0530

    Update patientinsert.php

--- patientinsert.php
+++ patientinsert.php
@@ -1,11 +1,7 @@
 <?php 
-
 require_once 'config.php';
 
-
      $postdata = file_get_contents("php://input");
-
-
      $request = json_decode($postdata);
      $Fname = mysqli_real_escape_string($conn,$request->Fname);
      echo json_encode("M IN INSERT PHP");
@@ -16,24 +12,18 @@ require_once 'config.php';
      $Phone=mysqli_real_escape_string($conn,$request->Phone);
      $Comment=mysqli_real_escape_string($conn,$request->Comment);
 
-echo json_encode($Gender);
+//echo json_encode($Gender);
     
     $sql = "INSERT INTO patient(Fname,Lname,Age,Dob,Gender,Phone,Comment)
       VALUES ('$Fname','$Lname',$Age,'$Dob','$Gender','$Phone','$Comment')";
 
-
 if($conn->query($sql)===TRUE)
 {
-    echo json_encode("TABLE inserted data successfully");
+    echo json_encode("Inserted data successfully");
 }
 else{
     header("HTTP/1.0 404 ERROR IN INSERT");
     exit;
   //  echo json_encode("Error INTO insertion" . $conn->error) ;
 }
-
-
-
-//ECHO json_encode("value");
-  
-  ?>
+?>
