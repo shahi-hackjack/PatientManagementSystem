463f1426cb63efbe5208b9c196dda31b4b0c4706
MOHAMMAD SHAHENOOR <contactshahenoor@gmail.com>
2016-11-30 01:00:49 +0530

    Update ctrl.js

--- ctrl.js
+++ ctrl.js
@@ -1,96 +1,86 @@
-var Patient=angular.module('Patient',[]);
+var Patient = angular.module('Patient', []);
 
-Patient.controller('mainctrl',['$scope','$http',function($scope,$http){ 
+Patient.controller('mainctrl', ['$scope', '$http', function($scope, $http) {
 
-console.log("M RUNNIG mainctrl WORKING");
-
-
-$scope.submitForm = function(isValid) {
- 
- 
-
-    if(isValid)
-    {console.log("check to make sure the form is completely valid !!!!!!");
-     console.log($scope.Patient);
-     //$scope.Patient="";
-
-    $http.post('patientinsert.php',$scope.Patient).success(function(res){
-           console.log(res);
-         $scope.Patient="";
-           document.getElementById('green').innerHTML="Successfull Attempt !!";
-             document.getElementById('red').innerHTML="";
-             
-
-           
-    }).error(function(){
-    console.log("ERROR IN INSERTION");
-       
-          $('#Gender').val('');
-          document.getElementById('red').innerHTML="Unsuccessfull Attempt!!";
-            document.getElementById('green').innerHTML="";
-    });
-
-         }
-               }//submit form ends here
+    console.log("Main controller working...");
 
 
-$scope.pform=function()
-{
- 
+    $scope.submitForm = function(isValid) {
 
-              $('#directory').hide();
-              $("#patient").show();
+            if (isValid) {
+                console.log("check to make sure the form is completely valid !!!!!!");
+                console.log($scope.Patient);
+                //$scope.Patient="";
 
-}
-
-
-$scope.totalCount = 0;
-
-$scope.directory=function()
-{
- 
-
-  $('#directory').height($("#patient").height());
-           
-            $("#patient").hide();
-           $('#directory').show();
-
-    $http.post('directoryload.php').success(function(res){
- console.log(res);
-$scope.totalCount = 0;
- $scope.PatientName=res;
-    }).error(function(){
-  
-   console.log("error")
-    });
-  
+                $http.post('patientinsert.php', $scope.Patient).success(function(res) {
+                    console.log(res);
+                    $scope.Patient = "";
+                    document.getElementById('green').innerHTML = "Successfull Attempt !!";
+                    document.getElementById('red').innerHTML = "";
 
 
 
-}
+                }).error(function() {
+                    console.log("ERROR IN INSERTION");
+
+                    $('#Gender').val('');
+                    document.getElementById('red').innerHTML = "Unsuccessfull Attempt!!";
+                    document.getElementById('green').innerHTML = "";
+                });
+
+            }
+        } //submit form ends here
 
 
-
-$scope.countInit = function() {
-   return $scope.totalCount++;
-}
-
-$scope.fulldetail=function(id)
-{
-	console.log(id);
-  $http.post('patientload.php',{'id':id}).success(function(res){
-
-       console.log(res[0].Fname);
-       $scope.PatientP=res;
+    $scope.pform = function() {
 
 
-  }).error(function(){
-  	console.log("ERROR");
+        $('#directory').hide();
+        $("#patient").show();
 
-  });
+    }
 
 
+    $scope.totalCount = 0;
+    $scope.directory = function() {
 
-}
+
+        $('#directory').height($("#patient").height());
+
+        $("#patient").hide();
+        $('#directory').show();
+
+        $http.post('directoryload.php').success(function(res) {
+            console.log(res);
+            $scope.totalCount = 0;
+            $scope.PatientName = res;
+        }).error(function() {
+
+            console.log("error")
+        });
+
+    }
+
+
+    $scope.countInit = function() {
+        return $scope.totalCount++;
+    }
+
+    $scope.fulldetail = function(id) {
+        console.log(id);
+        $http.post('patientload.php', {
+            'id': id
+        }).success(function(res) {
+
+            console.log(res[0].Fname);
+            $scope.PatientP = res;
+
+
+        }).error(function() {
+            console.log("ERROR");
+
+        });
+
+    }
 
 }]);
